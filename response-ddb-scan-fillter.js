dc32646f9a036ad3269a5d57fe687f2e43100710
dongocquy <dongocquy.online@gmail.com>
2020-07-17 06:28:43 +0700

    bo sung kieu scan co dieu kien

---
+++ response-ddb-scan-fillter.js
@@ -0,0 +1,31 @@
+console.log('Loading event');
+var AWS = require('aws-sdk');
+var ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = (event, context, callback) => {
+    console.log("Request received:\n", JSON.stringify(event));
+    console.log("Context received:\n", JSON.stringify(context));
+    var tableName = "VietRau-NhatKySanXuat";
+    var datetime = new Date().getTime().toString();
+    ddb.scan({
+        TableName: tableName,
+        FilterExpression: 'id = :id',
+        ExpressionAttributeValues: { 
+            ':id': 251
+        }
+    }, function(err, data) {
+        if (err) {
+            context.fail('ERROR: Dynamo failed: ' + err);
+        } else {
+            context.succeed({
+                statusCode: 201,
+        		body: JSON.stringify({
+        		    data
+        		}),
+        		headers: {
+        			'Access-Control-Allow-Origin': '*'
+        		}
+            });
+        }
+    });
+    
+}
